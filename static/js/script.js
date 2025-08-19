e49573c2f51092d3cd45a8695bd215ff4cff929f
Rok Kuzner <rok.ankaran@gmail.com>
2025-08-19 13:14:30 +0200

    scroll so the target paragraph is on the edge of the screen

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,15 @@
 const content_paragraphs = document.querySelectorAll(".content p")
 const target_paragraph = content_paragraphs[1]
-const mail_anchor = document.querySelector("#mail")
-
-console.log(target_paragraph.getBoundingClientRect())
 
 setTimeout(() => {
     // Get client rects for important elements
     let target_paragraph_rect = target_paragraph.getBoundingClientRect()
-    let mail_anchor_rect = mail_anchor.getBoundingClientRect()
 
     // Calculate center y value of the paragraph
-    const target_paragraph_center_y = target_paragraph_rect.top + (target_paragraph_rect.bottom - target_paragraph_rect.top)
+    const target_paragraph_center_y = target_paragraph_rect.top + (target_paragraph_rect.bottom - target_paragraph_rect.top) / 2
+
+    // If the target element is not already scrolled to
+    if (target_paragraph_center_y > window.innerHeight) {
+        window.scrollBy(0, target_paragraph_center_y-window.innerHeight)
+    }
 }, 5000)
