370a022b0a9fa25ca91781a353fac2979d22b2b9
Rok Kuzner <rok.ankaran@gmail.com>
2025-08-19 13:10:03 +0200

    add element rect calculations

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,13 @@ const content_paragraphs = document.querySelectorAll(".content p")
 const target_paragraph = content_paragraphs[1]
 const mail_anchor = document.querySelector("#mail")
 
+console.log(target_paragraph.getBoundingClientRect())
+
 setTimeout(() => {
-    console.log("Here!")
+    // Get client rects for important elements
+    let target_paragraph_rect = target_paragraph.getBoundingClientRect()
+    let mail_anchor_rect = mail_anchor.getBoundingClientRect()
+
+    // Calculate center y value of the paragraph
+    const target_paragraph_center_y = target_paragraph_rect.top + (target_paragraph_rect.bottom - target_paragraph_rect.top)
 }, 5000)
